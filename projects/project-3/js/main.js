d6f556e91c9fd254b90728829c4e4a886b3f18f8
tsegii tsendmaa <bnc3332@gmail.com>
2023-11-09 20:45:00 +0800

    add

--- projects/project-3/js/main.js
+++ projects/project-3/js/main.js
@@ -5,6 +5,9 @@ let closeBtn = document.querySelector('.closeBtn')
 
 let modal = document.querySelector('.sign-in-section');
 
+let dropDownMenu = document.querySelector('.dropDownMenu');
+
+let dropDownModal = document.querySelector('.dropDown')
 
 userBtn.addEventListener('click', () => {
     modal.classList.toggle('show');
@@ -13,3 +16,6 @@ closeBtn.addEventListener('click', () => {
     modal.classList.remove('show');
 })
 
+dropDownMenu.addEventListener('mouseover', () => {
+    dropDownModal.classList.toggle('show');
+})
